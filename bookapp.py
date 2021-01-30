f76c99ae11211fb49578ed5255ad898d1bbc8d65
tim <lurveyt@uw.edu>
2021-01-30 01:50:45 -0800

    bookapp finished

--- bookapp.py
+++ bookapp.py
@@ -1,23 +1,70 @@
 import re
-
+import pprint
 from bookdb import BookDB
 
 DB = BookDB()
 
 
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
+    book = DB.title_info(book_id)
+    return "\n".join(["<html>",
+                      "<head>",
+                      f"<title>{book.get('title')}</title>",
+                      f"<h1>{book.get('title')}</h1>"
+                      "</head>",
+                      "<body>",
+                      f"<p><b>Author: </b>{book.get('author')}</p>",
+                      f"<p><b>Publisher: </b>{book.get('publisher')}</p>",
+                      f"<p><b>ISDN: </b>{book.get('isdn')}</p>",
+                      "<a href='/'>Home</a>"
+                      "</body>",
+                      "</html>",
+                      ])
 
 
 def books():
-    return "<h1>a list of books</h1>"
+    """get the book titles from the database and put them into html"""
+    books = ""
+    for b in DB.titles():
+        books += f'<li>' \
+                 f'<a href="/book/{b.get("id")}">{b.get("title")}</a>' \
+                 f'</li>\n'
 
+    return "\n".join(["<html>",
+                      "<head>",
+                      "<title>A list of some book about stuff</title>",
+                      "<h1>Welcome to the books!</h1>"
+                      "</head>",
+                      "<body>",
+                      "<ul>",
+                      f"{books}",
+                      "</ul>",
+                      "</body>",
+                      "</html>",
+                      ])
 
 def application(environ, start_response):
     status = "200 OK"
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+
+    if environ.get('REQUEST_METHOD') == "GET":
+        # only two valid path formats
+        if environ.get('PATH_INFO') == "/":
+            # the root path
+            body = books()
+        elif re.match("/book/id\d+", environ.get('PATH_INFO')):
+            # the book path
+            body = book(book_id=environ.get('PATH_INFO').split("/")[-1])
+        else:
+            # bad path
+            status = "404 Not Found"
+            body = "NOT FOUND"
+    else:
+        body = "INVALID REQUEST"
+
+    response_headers = [('Content-Type', 'text/html'),
+                        ]
+    start_response(status, response_headers)
+    return [body.encode('utf8')]
 
 
 if __name__ == '__main__':
